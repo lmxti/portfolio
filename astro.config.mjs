22dabb97f9d7aa4dc74240977c0d47109a0ec096
lmxti <mvtias.smm@gmail.com>
2024-09-20 22:18:20 -0300

    update 24.09.20 fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://lmxti.github.io',
-  base: '/',
+  base: '/portfolio/',
   integrations: [tailwind(), react()]
 });
